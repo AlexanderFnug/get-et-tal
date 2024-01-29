28a55708c741dacbe09dc2daa7089c5b1165f33a
Alexander_Fnug <alexander.fnug-soltani@1508.dk>
2024-01-29 14:06:57 +0100

    functions

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ window.addEventListener('load', start)
 function start() {
     console.log('JS kører')
 }
+
+function generateNumber() {
+
+}
+
+function receiveGuess() {
+
+}
+
+function checkGuess() {
+
+}
+
+function guessCorrect() {
+
+}
+
+function guessTooHigh() {
+
+}
+
+function guessTooLow() {
+
+}
